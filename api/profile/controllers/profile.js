0e58cfb08ac86cef468b461959bd0869ec4cd664
kiet0807 <kiet080797@gmail.com>
2021-10-28 23:23:47 +0700

    fix

--- api/profile/controllers/profile.js
+++ api/profile/controllers/profile.js
@@ -1,12 +1,12 @@
 module.exports = {
   async create(ctx) {
-    const userReal = await strapi.services.token.findOne({
+    const userReal = await strapi.services.profile.findOne({
       user: ctx.state.user.id,
     });
     if (userReal) {
       return userReal.user;
     } else {
-      const userReal = await strapi.services.token.create({
+      const userReal = await strapi.services.profile.create({
         user: ctx.state.user.id,
       });
       return userReal.user;
